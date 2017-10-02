a31b7bfc706a1ea2e34d12338388dd8cbac1da42
Yasushi Asahi <asahi1600@gmail.com>
2017-10-02 23:34:48 +0900

    画面遷移の無い計算機

---
+++ app/src/main/java/jp/techacademy/yasushi/asahi/calcapp2/MainActivity.java
@@ -0,0 +1,58 @@
+package jp.techacademy.yasushi.asahi.calcapp2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.id.text1;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView answer;
+    EditText text1;
+    EditText text2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(this);
+
+        Button buttonDraw = (Button) findViewById(R.id.buttonDraw);
+        buttonDraw.setOnClickListener(this);
+
+        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonMultiply.setOnClickListener(this);
+
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        buttonDivide.setOnClickListener(this);
+
+        answer = (TextView) findViewById(R.id.answer);
+        text1 = (EditText) findViewById(R.id.text1);
+        text2 = (EditText) findViewById(R.id.text2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        double double1 = Double.parseDouble(text1.getText().toString());
+        double double2 = Double.parseDouble(text2.getText().toString());
+        double result;
+        if (v.getId() == R.id.buttonAdd) {
+            result = double1 + double2;
+        } else if (v.getId() == R.id.buttonDraw) {
+            result = double1 - double2;
+        } else if (v.getId() == R.id.buttonMultiply) {
+            result = double1 * double2;
+        } else {
+            result = double1 / double2;
+        }
+        answer.setText(String.valueOf(result));
+    }
+}
